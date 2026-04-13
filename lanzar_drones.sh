e9c5ccd3d3269db4123302bb3c233bcb9c847489
antsanni <antsanni@ucm.es>
2026-04-13 17:11:41 +0200

    lanzar_drones.sh
    Comando que pone en marcha a los drones

---
+++ lanzar_drones.sh
@@ -0,0 +1,38 @@
+echo "--- LANZADOR DE DRONES ---"
+
+while true; do
+    echo "Introduce el numero de drones que quieres utilizar:"
+    read NUM_DRONES
+
+    # Comprobamos que sea un número (sin letras) y que sea mayor que 0
+    if [[ "$NUM_DRONES" =~ ^[0-9]+$ ]] && [ "$NUM_DRONES" -gt 0 ]; then
+        break # ¡El número es válido! Salimos del bucle y continuamos
+    else
+        echo "❌ Error: Por favor, introduce un número entero mayor que 0."
+        echo "----------------------------------------"
+    fi
+done
+
+# Limpiamos si habia algo
+pkill -f sim_vehicle.py
+pkill -f arducopter
+
+cd ~/ardupilot || exit 1
+
+for (( i=0; i<NUM_DRONES; i++ ))
+do
+
+    OFFSET=$(echo "0.0001 * $i" | bc)
+    LAT=$(echo "28.761800 + $OFFSET" | bc)
+
+    echo " -> Dron $i listo (En puerto $i)"
+
+    # Comando que pone todo en marcha
+    nohup python3 Tools/autotest/sim_vehicle.py -v ArduCopter -f quad --no-mavproxy --instance $i --sysid $(($i+1)) --custom-location=$LAT,-17.741078,0,0 > /dev/null 2>&1 &
+
+    sleep 5
+done
+
+echo "----------------------------------------"
+echo "¡Drones listos!"
+echo "----------------------------------------"
